8afc546b7449daf9b949df34bc08aaa3ce613c03
Buchart <liam.buchart@nrcan-rncan.gc.ca>
2025-08-18 15:23:21 -0600

    update page with more install info

---
+++ docs/build/html/_sources/output/README.md.txt
@@ -0,0 +1,8 @@
+This is where you will find any of your prometheus spreadsheet output
+Naming Convention is as follows:
+
+Start_Time: datetimeobject
+String: prometheus_sheet
+Forecast_Length: hours
+
+`Start_Time_String_Forecast_Length`
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,7 +61,7 @@ html_theme = 'furo'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # something I found online to help me read jupyter notebooks
 numpydoc_show_class_members = False
